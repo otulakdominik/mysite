169857e4ce64ab08824f8c1ad5a7e977bc6054b3
otulakdominik <otulakdominik@gmail.com>
2018-05-11 18:56:21 +0200

    add models to articels

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_public=True)
+
+
+class Article(models.Model):
+    title = models.CharField(
+        'title',
+        max_length=255,
+    )
+
+    body = models.TextField(
+        'body',
+    )
+
+    created = models.DateField(
+        'created',
+        auto_now_add=True,
+    )
+
+    modified = models.DateField(
+        'modified',
+        auto_now=True,
+    )
+
+    pub_date = models.DateField(
+        'published date',
+        null=True,
+        blank=True,
+    )
+
+    comments_count = models.PositiveSmallInteger(
+        'comments count',
+        default=0,
+        editable=True,
+    )
+
+    is_public = models.BooleanField(
+        'is public',
+        default=False,
+    )
+
+    published = PublishedManager()
