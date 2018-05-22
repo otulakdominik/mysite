c00a6229775719c2f6851b7be8513da707584a0e
otulakdominik <otulakdominik@gmail.com>
2018-05-22 11:29:35 +0200

    add indexes to blog

---
+++ blog/search_indexes.py
@@ -0,0 +1,35 @@
+from haystack import indexes
+from .models import Entry
+
+
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    title = indexes.CharField(
+        document=True,
+        use_template=True,
+    )
+
+    body = indexes.TextField(
+        model_attr='body',
+    )
+
+    created = indexes.DateTimeField(
+        model_attr='created',
+    )
+
+    modified = indexes.DateTimeField(
+        model_attr='modified',
+    )
+
+    pub_date = indexes.DateTimeField(
+        model_attr='pub_date',
+    )
+
+    comments_count = indexes.PositiveSmallIntegerField(
+        model_attr='comments_count',
+    )
+
+    def get_model(self):
+        return Entry
+
+    def index_queryset(self, using=None):
+        return self.get_model().published
