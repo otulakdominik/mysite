e15fe8321f2215a2ef19302e075d09243bea6d16
otulakdominik <otulakdominik@gmail.com>
2018-06-01 13:07:10 +0200

    add models to product app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    class Meta:
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+    def __str__(self):
+        return self.name
